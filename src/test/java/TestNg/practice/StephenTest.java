4ab3f27e2d32d651977a50291fdcbcfd39bcf2ee
saykamstephen@gmail.com <saykamstephen@gmail.com>
2021-11-12 16:43:57 +0530

    Stephen Edited

---
+++ src/test/java/TestNg/practice/StephenTest.java
@@ -0,0 +1,12 @@
+package TestNg.practice;
+
+public class StephenTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Stephen Test");
+
+	}
+
+}
